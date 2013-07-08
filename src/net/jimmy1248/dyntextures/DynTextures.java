80ee9373c4713f4ac5ecb95df038efbfe709368e
Jimmy Allen <jimmy1248@gmail.com>
2013-07-08 18:05:47 +1200

    Initial Commit

---
+++ src/net/jimmy1248/dyntextures/DynTextures.java
@@ -0,0 +1,12 @@
+package net.jimmy1248.dyntextures;
+
+public class DynTextures {
+	private int[][] pixels;
+	private String texturepath;
+	public DynTextures(String texturepath) {
+		pixels = new int[16][16];
+		this.texturepath = texturepath;
+	}
+	
+	
+}
